f9808042cadd8dcc4ea6e0377b7d63b46820d9ff
Donald Linnemeyer <dlinnemeyer@economicmodeling.com>
2016-04-16 15:34:10 -0700

    started in on reduxform for consignor form

--- src/components/AddConsignorForm.jsx
+++ src/components/AddConsignorForm.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
 import {findDOMNode} from 'react-dom';
 import formSerialize from '../mixins/formSerialize';
+import {reduxForm} from 'redux-form';
 
-export default React.createClass({
+const fields = ["firstName", "lastName", "company", "isStoreAccount", "defaultPercSplit",
+      "address", "address2", "city", "state", "zip"];
 
-  mixins: [formSerialize],
+const AddConsignorForm = React.createClass({
+  render(){
+    const {
+      fields: { firstName, lastName, company, isStoreAccount, defaultPercSplit, address, address2,
+        city, state, zip },
+      error, resetForm, handleSubmit, submitting
+    } = this.props
 
-  fields: ["firstName", "lastName", "company", "isStoreAccount", "defaultPercSplit",
-      "address", "address2", "city", "state", "zip"],
-
-  submitHandler(event){
-    event.preventDefault();
-    this.props.onSubmit(this.serialize());
-  },
-
-  render: function() {
-    return <form onSubmit={this.submitHandler} ref="form">
+    return <form onSubmit={this.submitHandler}>
       <p>
-        <input type="text" defaultValue="" placeholder="First Name" name="firstName" ref="firstName" />
-        <input type="text" defaultValue="" placeholder="Last Name" name="lastName" ref="lastName" />
-        <input type="text" defaultValue="" placeholder="Company" name="company" ref="company" />
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Company" />
       </p>
       <p>
         <label>
-          <input type="checkbox" name="isStoreAccount" ref="isStoreAccount" />
+          <input type="checkbox" />
           Store Account
         </label>
-        <input type="text" defaultValue="" placeholder="Split" name="defaultPercSplit" ref="defaultPercSplit" />
+        <input type="text" placeholder="Split" />
       </p>
       <p>
-        <input type="text" defaultValue="" placeholder="Address" name="address" ref="address" />
-        <input type="text" defaultValue="" placeholder="Address 2" name="address2" ref="address2" />
+        <input type="text" placeholder="Address" />
+        <input type="text" placeholder="Address 2" />
       </p>
       <p>
-        <input type="text" defaultValue="" placeholder="City" name="city" ref="city" />
-        <input type="text" defaultValue="" placeholder="State" name="state" ref="state" />
-        <input type="text" defaultValue="" placeholder="Zip" name="zip" ref="zip" />
+        <input type="text" placeholder="City" />
+        <input type="text" placeholder="State" />
+        <input type="text" placeholder="Zip" />
       </p>
       <p><input type="submit" value="Add Consignor" /></p>
       {this.props.isLoading && <img src="/img/loading.gif" />}
@@ -43,3 +42,15 @@ export default React.createClass({
   }
 });
 
+AddConsignorForm.propTypes = {
+  fields: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  resetForm: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired
+}
+
+export default reduxForm({
+  form: 'addConsignor',
+  fields
+})(AddConsignorForm);
