2f4880ac55b69bff4451f48f2267a9e1439e7263
Donald Linnemeyer <dlinnemeyer@economicmodeling.com>
2016-05-10 10:45:07 -0700

    some work on moving items over

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,58 +1,88 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import {connect, withRouter} from 'react-redux';
 import ItemList from './ItemList';
-import {loadItems, addFakeItems, deleteAllItems} from '../actions/items.js';
+import ConsignorListFilter from './ConsignorListFilter';
+import {searchItems, addFakeItems, deleteAllItems} from '../actions/items.js';
 import {loading} from '../actions/general.js';
 import {Link} from 'react-router';
 import InnerLoading from './InnerLoading'
-
-const loadingId = "itemslist";
+import Pagination from './Pagination'
+import {isEqual} from 'lodash/fp'
+import {asyncify} from '../lib/asyncify'
 
 export const Items = React.createClass({
 
+  setUserSettings(data){
+    const query = Object.assign({}, this.props.userSettings, data);
+    history.push({pathname: '/consignors', query});
+    this.loadConsignors(query);
+  },
+
+  lastSettingsLoad: undefined,
+  loadItems(settings, force = false){
+    if(!settings) settings = this.props.userSettings || {};
+    if(!force && isEqual(this.lastSettingsLoad, settings)) return Promise.resolve();
+
+    this.lastSettingsLoad = settings;
+
+    const { filters, sortBy, page } = settings;
+    return this.props.items.load(filters, sortBy, {perPage: 30, page});
+  },
+
   componentWillMount(){
-    this.props.loading(loadingId, true);
-    this.props.loadItems()
-      .then(items=> {
-        this.props.loading(loadingId, false);
-      });
+    this.loadItems();
+  },
+
+  componentWillReceiveProps(){
+    this.loadItems();
+  },
+
+  onFilterSubmit(data){
+    this.setUserSettings({filters: data});
+  },
+
+  paginate(page){
+    this.setUserSettings({page});
+  },
+
+  sort(sortBy){
+    this.setUserSettings({sortBy});
   },
 
   render() {
-    const {isLoading, items, addFakeItems, deleteAllItems} = this.props;
+    const {items, addFakeItems, deleteAllItems, userSettings} = this.props;
     const _addFakeItems = e => { e.preventDefault(); addFakeItems(); }
     const _deleteAllItems = e => { e.preventDefault(); deleteAllItems(); }
     return <div>
       <Link to="/items/new">Add Item</Link><br />
+      <Link to={{pathname: "/items", query: {"printed": "0"}}}>Unprinted Items</Link><br />
       <a href="#" onClick={_addFakeItems}>Add Lots O' Items</a><br />
       <a href="#" onClick={_deleteAllItems}>Delete All</a>
-      {isLoading
+      {items.loading
         ? <InnerLoading />
-        : <ItemList items={items} />}
+        : <ItemList items={items.data} />}
     </div>;
   }
 
 });
 
-function mapStateToProps(state){
+function mapStateToProps(state, props){
+  const userSettings = Object.assign({
+    page: "1",
+    sortBy: "sku",
+    filters: {}
+  }, props.location.query);
+
+  const consignors = props.search.data.ids.map(id => state.consignors[id]);
+
   return {
-    // pull from state just pulls all items, which is obviously bad. we need to instead store
-    // some page-related state for this Items page, including pagination, filters, and current
-    // displayed itemids.
-    // We can then load just those itemids into the state's item model repo, then pull the details
-    // from there.
-    // Remember, there's a big difference between async data pulls (loadItems, which populates
-    // the state's model repo) and grabbing data from state (getItems, which pulls from the state's
-    // model repo). I wonder if there's a way to blend the two so component wrappers
-    // don't have to care about the difference? though probably not. component wrappers probably
-    // need to know their own state structure, which means they have to know they're only storring
-    // id's in their state, not full objects. which means they have to know the difference between
-    // hydrating a model repo and pulling from the repo based on id
-    items: state.items,
-    isLoading: state.loading[loadingId]
-  }
+    consignors,
+    userSettings
+  };
 }
 
-export const ItemsContainer = connect(mapStateToProps, {
-  loadItems, loading, addFakeItems, deleteAllItems
-})(Items);
+const ReduxedItems = connect(mapStateToProps)(withRouter(Items));
+
+export const ItemsContainer = asyncify(ReduxedConsignors, "itemslist", {
+  "items": { data: {}, loading: true, load: searchItems }
+});
