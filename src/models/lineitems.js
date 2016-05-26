015d912fb5495bc9b1fd04435411bab2ea2274b5
Donald Linnemeyer <dlinnemeyer@economicmodeling.com>
2016-05-25 18:51:31 -0700

    got lineitem tests passing for global discounts

--- src/models/lineitems.js
+++ src/models/lineitems.js
@@ -1,13 +1,21 @@
-/* @flow */
 import {sumBy} from 'lodash'
 import {run, price, listOf} from '../lib/declarmath/base'
 import {TaxSet} from './taxes'
 import {DiscountSet} from './discounts'
 
-// TODO: consider breaking this into smaller functions that we just merge together in a parent
-// function? easier to test and reason about that way
-const _LineItem = ({listPrice = 0, customerSurcharge = 0, discounts = [], taxes = [], percentSplit = 0, consignorSurcharge = 0, ...meta}) => ({
-  ...meta, //title, description, silly stuff like that
+// Line Items are tough because in applying global fixed discounts, we have to distribute those
+// discounts amoung all items. But they have to be distributed based on post-line-item-discount
+// ratios and before taxes (since discount amounts aren't taxable).
+// This means we have to calculate up to line-item-discount level, then step back out, calculate
+// ratios for the whole list, then step back into each line item to apply distributed global
+// discounts, taxes, and calculate splits.
+// This means line item calculations aren't pure; they have to know about each other (or be passed
+// their ratio of the discounted subtotal). So solve this, we broke the different portions of line
+// item calculations into separate functions, each of which is responsible for a single step
+// in the calculations. We merge them together in the end to create the complete line item model.
+
+const _Discounted = ({listPrice = 0, customerSurcharge = 0, discounts = [], ...other}) => ({
+  ...other, // for meta info. also for fields that will be used later in _GlobalDiscounted
 
   // basics
   listPrice:              price(listPrice),
@@ -19,17 +27,29 @@ const _LineItem = ({listPrice = 0, customerSurcharge = 0, discounts = [], taxes 
   // TODO: store vs. normal
   discountSet:            o => DiscountSet({discounts, input: o.receiptPrice}),
   discountAmount:         o => o.discountSet.amount,
-  discountedPrice:        o => o.discountSet.output,
+  discountedPrice:        o => o.discountSet.output
+})
+
+// take a _Discounted line item and apply more discounts
+// also handles the rest of the operations, since we don't have to step out of the list anymore
+// at this point. we could split this more into _Taxed, and _Split, which might be nice, but I'll
+// save that for later
+const _GlobalDiscounted = ({receiptPrice, globalDiscounts = [], taxes = [], percentSplit = 0, consignorSurcharge = 0, ...Discounted}) => ({
+  ...Discounted,
+
+  globalDiscountSet:      o => DiscountSet({discounts: globalDiscounts, input: o.discountedPrice}),
+  globalDiscountAmount:   o => o.globalDiscountSet.amount,
+  globalDiscountedPrice:  o => o.globalDiscountSet.output,
 
   // taxes
   // TODO: inclusive vs. exclusive
-  taxSet:                 o => TaxSet({taxes, input: o.discountedPrice}),
+  taxSet:                 o => TaxSet({taxes, input: o.globalDiscountedPrice}),
   taxAmount:              o => o.taxSet.amount,
   taxedPrice:             o => o.taxSet.output,
 
   // various clarifying totals
   // TODO: nonTaxPrice will have to be different once we add exclusive taxes
-  nonTaxedPrice:          o => o.discountedPrice,
+  nonTaxedPrice:          o => o.globalDiscountedPrice,
   total:                  o => o.taxedPrice,
 
   // split calculations. hold onto your butts...
@@ -47,13 +67,65 @@ const _LineItem = ({listPrice = 0, customerSurcharge = 0, discounts = [], taxes 
   netStorePortion:        o => o.grossStorePortion
 })
 
-const _LineItemSet = ({lineItems}) => ({
-  lineItems:          lineItems.map(_LineItem),
-  discountAmount:     o => sumBy(o.lineItems, 'discountAmount'),
-  discountedTotal:    o => sumBy(o.lineItems, 'discountedPrice'),
-  taxAmount:          o => sumBy(o.lineItems, 'taxAmount'),
-  taxedTotal:         o => sumBy(o.lineItems, 'taxedPrice'),
+
+// take an array of global discounts, an array of line items, and a subtotal, and returns a list
+// of lineItems with globals added
+const distributeGlobals = (globalDiscounts, lineItems, subtotal) => {
+  return globalDiscounts.map(discount => distributeGlobal(discount, lineItems, subtotal))
+}
+const distributeGlobal = (discount, lineItems, subtotal) => {
+  // percent discounts don't change at all
+  if(discount.type == 'percent') return lineItems.map(() => discount)
+
+  // and now the cause for all this mess: distributing fixed discounts based on ratio
+  let remainder = discount.value
+  return lineItems.map((lineItem, i) => {
+    const value = (lineItems.length - 1) == i
+      // grab the remaining if we're on the last item so we don't leave hanging pennies
+      ? remainder
+      // make sure to round here, so we're distributing by the penny
+      : price(remainder * (lineItem.discountedPrice / subtotal))
+
+    remainder -= value
+    return {type: 'fixed', value}
+  })
+}
+
+// takes discounted line items and distributed globals and gives back rendered line items
+const applyGlobalDiscounts = (lineItems, distributedGlobals) => {
+  return lineItems.map((li, i) => {
+    // global discounts should be ordered the same as lineItems, so form an array of discounts
+    // by selected based on index from distributedGlobals
+    const globalDiscounts = distributedGlobals.map(discounts => discounts[i])
+    return _GlobalDiscounted({...li, globalDiscounts})
+  })
+}
+
+
+// to calculate discounts
+// - calculate DiscountedLineItems, which calculates the line-item-specific discounts
+// - calculate discountedSubtotal
+// - calculate GlobalDiscountedLineItems, which add global discounts.
+//    take discountedSubtotal, DiscountedLineItems, and global discounts. alter global fixed
+//    discounts by allocating by % of discountedSubtotal. Make sure the last item "uses up" the
+//    remainder of the fixed amount.
+// - calculate lineItems, which takes globaldiscountedlineitems and adds taxes and split calculations
+
+const _LineItemSet = ({lineItems, globalDiscounts = [], taxes = []}) => ({
+  discounted:             lineItems.map(_Discounted),
+  discountAmount:         o => sumBy(o.discounted, 'discountAmount'),
+  discountedTotal:        o => sumBy(o.discounted, 'discountedPrice'),
+
+  distributedGlobals:     o => distributeGlobals(globalDiscounts, o.discounted, o.discountedTotal),
+  globalDiscounted:       o => applyGlobalDiscounts(o.discounted, o.distributedGlobals),
+  globalDiscountAmount:   o => sumBy(o.globalDiscounted, 'globalDiscountAmount'),
+  globalDiscountedTotal:  o => sumBy(o.globalDiscounted, 'globalDiscountedPrice'),
+
+  lineItems:              o => o.globalDiscounted,
+  taxAmount:              o => sumBy(o.lineItems, 'taxAmount'),
+  taxedTotal:             o => sumBy(o.lineItems, 'taxedPrice'),
 })
 
-export const LineItem = run(_LineItem)
+export const Discounted = run(_Discounted)
+export const GlobalDiscounted = run(_GlobalDiscounted)
 export const LineItemSet = run(_LineItemSet)
--- src/models/lineitems.test.js
+++ src/models/lineitems.test.js
@@ -1,107 +1,114 @@
 import {assert} from 'chai'
-import {LineItem, LineItemSet} from './lineitems'
+import {Discounted, GlobalDiscounted, LineItemSet} from './lineitems'
 import {DiscountSet} from './discounts'
 import {TaxSet} from './taxes'
 
 describe('LineItems', () => {
-  describe('LineItem', () => {
-
+  describe('Discounted', () => {
     it('should calculate a basic line item', () => {
-      assert.deepEqual(LineItem({title: 'some title', listPrice: 353, percentSplit: 43}),{
-        title: 'some title',
-        listPrice: 353,
-        percentSplit: 43,
-        customerSurcharge: 0,
-        receiptPrice: 353,
-
-        discountSet: DiscountSet({discounts: [], input: 353}),
-        discountAmount: 0,
-        discountedPrice: 353,
-
-        taxSet: TaxSet({taxes: [], input: 353}),
-        taxAmount: 0,
-        taxedPrice: 353,
-
-        nonTaxedPrice: 353,
-        total: 353,
-
-        grossSplitPrice: 353,
-        netSplitPrice: 353,
-        percentSplit: 43,
-        grossConsignorPortion: 152,
-        consignorSurcharge: 0,
-        netConsignorPortion: 152,
-        grossStorePortion: 201,
-        netStorePortion: 201
-      })
     })
 
-    it('should calculate a line item with the fixins', () => {
-      const rawLineItem = {
-        title: 'some title',
-        description: 'description',
-        listPrice: 353,
-        customerSurcharge: 100,
-        discounts: [{value: 50, type: 'fixed'}, {value: 10, type: 'percent'}],
-        taxes: [{rate: 4}, {rate: 6}],
-        percentSplit: 56.4,
-        consignorSurcharge: 30
-      }
-      assert.deepEqual(LineItem(rawLineItem), {
-        title: 'some title',
-        description: 'description',
-        listPrice: 353,
-        customerSurcharge: 100,
-        receiptPrice: 453,
-
-        discountSet: DiscountSet({discounts: rawLineItem.discounts, input: 453}),
-        discountAmount: 90,
-        discountedPrice: 363,
-
-        taxSet: TaxSet({taxes: rawLineItem.taxes, input: 363}),
-        taxAmount: 37,
-        taxedPrice: 400,
-
-        nonTaxedPrice: 363,
-        total: 400,
-
-        grossSplitPrice: 363,
-        netSplitPrice: 263,
-        percentSplit: 56.4,
-        grossConsignorPortion: 148,
-        consignorSurcharge: 30,
-        netConsignorPortion: 118,
-        grossStorePortion: 115,
-        netStorePortion: 115
-      })
-
+    it('should calculate some discounts', () => {
     })
   })
 
+  describe('GlobalDiscounted', () => {
+    it('should calculate a basic line item', () => {
+    })
+
+    it('should calculate some discounts', () => {
+    })
+  })
+
+  // TODO add tests for Taxed, and Split
+
+  // since we've tested other functionality above, we mainly just need to test globalDiscounts
+  // for line item sets, and the derived aggregate values
   describe('LineItemSet', () => {
 
     it('should handle an empty lineitem list', () => {
       assert.deepEqual(LineItemSet({lineItems: []}), {
-        lineItems: [],
+        discounted: [],
         discountAmount: 0,
         discountedTotal: 0,
+
+        distributedGlobals: [],
+        globalDiscounted: [],
+        globalDiscountAmount: 0,
+        globalDiscountedTotal: 0,
+
+        lineItems: [],
         taxAmount: 0,
         taxedTotal: 0
       })
     })
 
-    it('should handle a couple lineitems', () => {
-      const rawLineItems = [
-        {listPrice: 500, discounts: [{type: 'fixed', value: 200}], taxes: [{rate: 5.5}]},
-        {listPrice: 1000, discounts: [{type: 'percent', value: 50}], taxes: [{rate: 10}]},
-      ]
-      assert.deepEqual(LineItemSet({lineItems: rawLineItems}),{
-        lineItems: rawLineItems.map(LineItem),
+    it('should handle a couple lineitems with no global discounts or taxes', () => {
+      const rawLineItemSet = {
+        lineItems: [
+          {listPrice: 500, discounts: [{type: 'fixed', value: 200}], taxes: [{rate: 5.5}]},
+          {listPrice: 1000, discounts: [{type: 'percent', value: 50}], taxes: [{rate: 10}]},
+        ],
+        globalDiscounts: [],
+        taxes: []
+      }
+      const discounted = rawLineItemSet.lineItems.map(Discounted)
+      const globalDiscounted = discounted.map(GlobalDiscounted)
+      assert.deepEqual(LineItemSet(rawLineItemSet),{
+        discounted,
         discountAmount: 700,
         discountedTotal: 800,
+
+        distributedGlobals: [],
+        globalDiscounted,
+        globalDiscountAmount: 0,
+        globalDiscountedTotal: 800,
+
+        lineItems: globalDiscounted,
         taxAmount: 67,
         taxedTotal: 867
       })
     })
+
+    it('should handle a couple lineitems with global discounts', () => {
+      const rawLineItemSet = {
+        lineItems: [
+          {listPrice: 500, discounts: [{type: 'fixed', value: 200}], taxes: [{rate: 5.5}]},
+          {listPrice: 1000, discounts: [{type: 'percent', value: 50}], taxes: [{rate: 10}]},
+        ],
+        globalDiscounts: [
+          {type: 'percent', value: 10},
+          {type: 'fixed', value: 250}
+        ],
+        taxes: []
+      }
+      const discounted = rawLineItemSet.lineItems.map(Discounted)
+      const distributedGlobals = [
+        [{type: 'percent', value: 10}, {type: 'percent', value: 10}],
+        [{type: 'fixed', value: 94}, {type: 'fixed', value: 156}]
+      ]
+      const globalDiscounted = discounted.map((li, i) => {
+        return GlobalDiscounted({...li, globalDiscounts: distributedGlobals.map(a => a[i])})
+      })
+
+      assert.deepEqual(LineItemSet(rawLineItemSet),{
+        discounted,
+        discountAmount: 700,
+        discountedTotal: 800,
+
+        distributedGlobals,
+        globalDiscounted,
+        globalDiscountAmount: 330,
+        globalDiscountedTotal: 470,
+
+        lineItems: globalDiscounted,
+        taxAmount: 39,
+        taxedTotal: 509
+      })
+    })
+
+    it('should handle a couple lineitems with global discounts and taxes', () => {
+    })
+
   })
 })
